4fa13503d81b932c06534ef1f0cf36aa524bbdaf
Madoks Charles <111252523+madokscharles@users.noreply.github.com>
2023-05-04 22:17:58 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# This manifest creates a file with puppet
-file { '/tmp/school':
-    ensure  => 'file',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# A manifest that executes a bash command
-# bash commnad kills a process
-exec { 'kill':
-  command => 'pkill -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin']
-}
