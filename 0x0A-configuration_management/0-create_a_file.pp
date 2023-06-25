857664ab5f1d5e81ad84171366704cee1f929df8
madokscharles <charlesmadoks@gmail.com>
2023-06-25 15:25:28 -0700

    Creates a file in /tmp using Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This creates a file at /tmp using puppet
+
+file { '/tmp/school':
+    ensure  => 'file',
+    path    => '/tmp/school',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
