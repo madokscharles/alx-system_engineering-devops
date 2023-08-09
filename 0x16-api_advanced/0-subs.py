9e4b1d87d41bf47c63b7e8eafa046fd24bd876b8
madokscharles <charlesmadoks@gmail.com>
2023-08-08 20:06:12 -0700

    Fix PEP8 error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Function queries the Reddit API, 
+Function queries the Reddit API,
 Returns the number of sunscribers (not active users, total subs)
 """
 
@@ -12,8 +12,9 @@ def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
 
-    response = get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                headers = {'User-Agent': '0x16-api_advanced:project:\
+    response = get(
+        'http://www.reddit.com/r/{}/about.json'.format(subreddit),
+        headers={'User-Agent': '0x16-api_advanced:project:\
                         v1.0.0'}).json()
     subs = response.get("data", {}).get("subscribers", 0)
     return subs
