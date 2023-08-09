0aa515174ac1f07b51af20fcd865ff5c987aa2b4
madokscharles <charlesmadoks@gmail.com>
2023-08-08 22:02:08 -0700

    Fix checker errors

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,15 +4,28 @@ A recursive function that queries the Reddit API
 Parses the title of all hot articles and prints a sorted
 count of given keywords
 """
-import json
+import re
 from requests import get
+import sys
 
 
-def count_words(subreddit, word_list, after="", count=[]):
-    """Counting all words"""
-    if after == "":
-        count = [0] * len(word_list)
+def add_title(dictionary, hot_posts):
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
+        return
 
+    title = hot_posts[0]['data']['title'].split()
+    for word in title:
+        for key in dictionary.keys():
+            c = re.compile("^{}$".format(key), re.I)
+            if c.findall(word):
+                dictionary[key] += 1
+    hot_posts.pop(0)
+    add_title(dictionary, hot_posts)
+
+
+def recurse(subreddit, dictionary, after=None):
+    """Queries to Reddit API"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
                 'User-Agent': '0x16-api_advanced:project:v1.0.0'
@@ -24,38 +37,33 @@ def count_words(subreddit, word_list, after="", count=[]):
                 url, headers=headers, params=params,
                 allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
+    if response.status_code != 200:
+        return None
 
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
+    dic = response.json()
+    hot_posts = dic['data']['children']
+    add_title(dictionary, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return
+    recurse(subreddit, dictionary, after=after)
 
-        after = data['data']['after']
-        if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
 
-                for i in range(len(word_list)):
-                    for j in range(i, len(word_list)):
-                        if (count[j] > count[i] or
-                                (word_list[i] > word_list[j] and
-                                    count[j] == count[i])):
-                            aux = count[i]
-                            count[i] = count[j]
-                            count[j] = aux
-                            aux = word_list[i]
-                            word_list[i] = word_list[j]
-                            word_list[j] = aux
+def count_words(subreddit, word_list):
+    """ Init function """
+    dictionary = {}
 
-                for i in range(len(word_list)):
-                    if (count[i] > 0) and i not in save:
-                        print("{}: {}".format(word_list[i].lower(), count[i]))
-        else:
-            count_words(subreddit, word_list, after, count)
+    for word in word_list:
+        dictionary[word] = 0
+
+    recurse(subreddit, dictionary)
+
+    length = sorted(dictionary.items(), key=lambda kv: kv[1])
+    length.reverse()
+
+    if len(length) != 0:
+        for item in length:
+            if item[1] != 0:
+                print("{}: {}".format(item[0], item[1]))
+    else:
+        print("")
